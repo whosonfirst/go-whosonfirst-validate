3dd4406f209a0356493c860a8ff1f461c3c10b8e
thisisaaronland <devnull@localhost>
2019-06-20 16:08:59 -0700

    update to support Go modules

---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/whosonfirst/go-whosonfirst-validate
+
+require (
+	github.com/hashicorp/go-multierror v0.0.0-20171204182908-b7773ae21874
+	github.com/mmcloughlin/geohash v0.9.0
+	github.com/pkg/errors v0.8.0
+	github.com/skelterjohn/geom v0.0.0-20180103000000-96f3e8a219c5f4276b0dda3568d80c4e02a50116
+	github.com/tidwall/gjson v1.2.1
+	github.com/whosonfirst/go-rfc-5646 v0.1.0
+	github.com/whosonfirst/go-whosonfirst-crawl v0.1.0
+	github.com/whosonfirst/go-whosonfirst-csv v0.1.0
+	github.com/whosonfirst/go-whosonfirst-flags v0.1.0
+	github.com/whosonfirst/go-whosonfirst-geojson-v2 v0.10.2
+	github.com/whosonfirst/go-whosonfirst-hash v0.1.0
+	github.com/whosonfirst/go-whosonfirst-index v0.1.2
+	github.com/whosonfirst/go-whosonfirst-log v0.1.0
+	github.com/whosonfirst/go-whosonfirst-names v0.1.0
+	github.com/whosonfirst/go-whosonfirst-placetypes v0.1.0
+	github.com/whosonfirst/go-whosonfirst-spr v0.1.0
+	github.com/whosonfirst/go-whosonfirst-sqlite v0.1.0
+	github.com/whosonfirst/go-whosonfirst-uri v0.1.0
+	github.com/whosonfirst/warning v0.1.0
+
+)
+
+go 1.12
--- vendor/github.com/whosonfirst/go-whosonfirst-geojson-v2/properties/whosonfirst/whosonfirst.go
+++ vendor/github.com/whosonfirst/go-whosonfirst-geojson-v2/properties/whosonfirst/whosonfirst.go
@@ -114,6 +114,15 @@ func Concordances(f geojson.Feature) (WOFConcordances, error) {
 	return concordances, nil
 }
 
+func Source(f geojson.Feature) string {
+
+	possible := []string{
+		"properties.src:geom",
+	}
+
+	return utils.StringProperty(f.Bytes(), possible, "unknown")
+}
+
 func Country(f geojson.Feature) string {
 
 	possible := []string{
@@ -262,6 +271,15 @@ func LastModified(f geojson.Feature) int64 {
 	}
 
 	return utils.Int64Property(f.Bytes(), possible, -1)
+}
+
+func IsAlt(f geojson.Feature) bool {
+
+	if ParentId(f) == -1 {
+		return true
+	}
+
+	return false
 }
 
 func IsCurrent(f geojson.Feature) (flags.ExistentialFlag, error) {
@@ -487,6 +505,60 @@ func BelongsToOrdered(f geojson.Feature) ([]int64, error) {
 	}
 
 	return belongs_to, nil
+}
+
+func BelongsToWithCeiling(f geojson.Feature, str_pt string) ([]int64, error) {
+
+	pt, err := placetypes.GetPlacetypeByName(str_pt)
+
+	if err != nil {
+		return nil, err
+	}
+
+	valid := make(map[string]bool)
+	depicts := make(map[int64]bool)
+
+	roles := []string{
+		"common",
+		"common_optional",
+		"optional",
+	}
+
+	descendants := placetypes.DescendantsForRoles(pt, roles)
+
+	for _, p := range descendants {
+		valid[p.Name] = true
+	}
+
+	hierarchies := Hierarchies(f)
+
+	for _, h := range hierarchies {
+
+		for k, id := range h {
+
+			_, ok := depicts[id]
+
+			if ok {
+				continue
+			}
+
+			k = strings.Replace(k, "_id", "", 1)
+
+			_, ok = valid[k]
+
+			if ok {
+				depicts[id] = true
+			}
+		}
+	}
+
+	ids := make([]int64, 0)
+
+	for id, _ := range depicts {
+		ids = append(ids, id)
+	}
+
+	return ids, nil
 }
 
 func IsBelongsTo(f geojson.Feature, id int64) bool {
